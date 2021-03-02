960b69d9dd20fd3e6672d40e2b05e196baa1107c
elgata33 <garashov123@gmail.com>
2021-03-02 19:47:57 +0300

    feat: finish test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array) {
+    let minValue = 0 / 1;
+    if (typeof array === "undefined" || array.length == 0) {
+        return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (minValue > array[i]) {
+            minValue = array[i];
+        }
+    }
+    return minValue;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let maxValue = 0;
+    if (typeof array === "undefined" || array.length == 0) {
+        return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (maxValue < array[i]) {
+            maxValue = array[i];
+        }
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    return maxValue;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let avgValue;
+    let sum = 0;
+    if (typeof array === "undefined" || array.length == 0) {
+        return 0;
+    }
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return (avgValue = sum / array.length);
+};
